3902e3baec6c19e97acb473fc0d42a37806f79d3
Mathew Gardiner <30789024+mathewgardiner@users.noreply.github.com>
2018-08-20 16:09:17 -0700

    Added pull_upstream files

---
+++ pull_upstream.sh
@@ -0,0 +1,14 @@
+# 1. Follow steps in pull_upstream.md
+
+# 2. It is a good idea to delete the thesis.pdf first as binary files can't
+# 	 be merged. Commit all changes.
+
+# 2. Add a convinient directory for backups below
+dir="/mnt/c/Users/mgardine/Documents/Thesis backups/"
+
+# 3. Make a backup
+mkdir "$dir$(date +"%F_%H-%M-%S")"
+cp -r . "$dir$(date +"%F_%H-%M-%S")"
+
+# 4. Do a pull and resolve conflicts if nessecary
+git pull template master
