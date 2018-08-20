75c2261864a3ae6d3c9e10c6ffb84b68f5fa0ae0
Mathew Gardiner <30789024+mathewgardiner@users.noreply.github.com>
2018-08-20 16:15:41 -0700

    Made username generic

--- pull_upstream.sh
+++ pull_upstream.sh
@@ -4,7 +4,7 @@
 # 	 be merged. Commit all changes.
 
 # 2. Add a convinient directory for backups below
-dir="/mnt/c/Users/mgardine/Documents/Thesis backups/"
+dir="/mnt/c/Users/<username>/Documents/Thesis backups/"
 
 # 3. Make a backup
 mkdir "$dir$(date +"%F_%H-%M-%S")"
